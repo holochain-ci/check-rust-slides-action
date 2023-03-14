ae6a5661d0f5abd0ae23a22d2767a7b9778d4e5d
Harlan Wood <harlan.wood@sharespost.com>
2023-03-13 21:16:18 -1000

    Create and build rust files

--- check.ts
+++ check.ts
@@ -1,24 +1,43 @@
-import { JSDOM } from "jsdom";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
+import { JSDOM } from 'jsdom'
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import { run } from './run'
 
-const filePath = path.join(__dirname, "index.html");
-const html = fs.readFileSync(filePath, "utf8");
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-const dom = new JSDOM(html);
+const filePath = path.join(__dirname, 'index.html')
+const html = fs.readFileSync(filePath, 'utf8')
 
-const popovers = dom.window.document.querySelectorAll("span.popover");
+const dom = new JSDOM(html)
+
+const popovers = dom.window.document.querySelectorAll('span.popover')
 for (const popover of popovers) {
-  popover.remove();
+	popover.remove()
 }
 
-const rusts = dom.window.document.querySelectorAll("code.rust");
+const rusts = dom.window.document.querySelectorAll('code.rust')
 
+const HAPP_DIR = `${__dirname}/holochain-sandbox`
+const SRC_DIR = `${HAPP_DIR}/dna/src`
+const LIB_PATH = `${SRC_DIR}/lib.rs`
+
+run(`rm -rf ${LIB_PATH}`)
+run(`touch ${LIB_PATH}`)
+run(`rm -rf ${SRC_DIR}/slide_*`)
+
+let i = 1
 for (const rust of rusts) {
-  const code = rust.textContent;
-  console.log(code);
-  console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+	const code = rust.textContent || ''
+	const filename = `${SRC_DIR}/slide_${i}.rs`
+	fs.writeFileSync(filename, code)
+
+	let lib = fs.readFileSync(LIB_PATH, 'utf8')
+	lib = `${lib}\nmod slide_${i};`
+	fs.writeFileSync(LIB_PATH, lib)
+
+	i++
 }
+
+run(`cd ${HAPP_DIR} && nix develop --command bash -c "cargo build --color always"`)
